bf8c39f9bc9e783fb095848ea90443fec38ebe82
rkn2 <nap@psu.edu>
2025-11-26 13:48:50 -0500

    Update Lua filter to merge consecutive citation markers with commas

--- endnotes.lua
+++ endnotes.lua
@@ -1,25 +1,66 @@
 -- Lua filter to convert footnotes to endnotes (static text)
 -- This collects all footnotes and places them at the end of the document.
+-- It also merges consecutive footnotes into a single superscript marker (e.g., 1,2,3).
 
 local List = require 'pandoc.List'
 local utils = require 'pandoc.utils'
 
 local notes = List:new()
 
-function Note(el)
-  -- Get the index for this note
-  local num = #notes + 1
-  
-  -- Store the note content
-  notes[num] = el.content
-  
-  -- Return a superscript number linking to the endnote
-  -- We create a link to the endnote anchor
-  local anchor = "endnote-" .. num
-  local backlink = "ref-" .. num
-  
-  -- Create the marker in the text: superscript number
-  return pandoc.Superscript(pandoc.Link(pandoc.Str(tostring(num)), "#" .. anchor))
+function Inlines(inlines)
+  local new_inlines = List:new()
+  local i = 1
+  while i <= #inlines do
+    local el = inlines[i]
+    if el.t == 'Note' then
+      -- Found a note, check for subsequent notes
+      local current_indices = {}
+      
+      -- Process this note
+      local num = #notes + 1
+      notes[num] = el.content
+      table.insert(current_indices, num)
+      
+      -- Look ahead for more notes
+      local j = i + 1
+      while j <= #inlines do
+        local next_el = inlines[j]
+        if next_el.t == 'Note' then
+          local next_num = #notes + 1
+          notes[next_num] = next_el.content
+          table.insert(current_indices, next_num)
+          j = j + 1
+        elseif next_el.t == 'Space' then
+           -- Skip spaces between notes
+           j = j + 1
+        elseif next_el.t == 'Str' and next_el.text == ',' then
+            -- Skip commas between notes (if they exist in source)
+            j = j + 1
+        else
+           break
+        end
+      end
+      
+      -- Create the combined marker
+      local marker_inlines = List:new()
+      for k, idx in ipairs(current_indices) do
+        if k > 1 then
+          marker_inlines:insert(pandoc.Str(",")) -- Comma separator
+        end
+        local anchor = "endnote-" .. idx
+        marker_inlines:insert(pandoc.Link(pandoc.Str(tostring(idx)), "#" .. anchor))
+      end
+      
+      new_inlines:insert(pandoc.Superscript(marker_inlines))
+      
+      -- Advance main loop
+      i = j
+    else
+      new_inlines:insert(el)
+      i = i + 1
+    end
+  end
+  return new_inlines
 end
 
 function Pandoc(doc)
@@ -35,18 +76,8 @@ function Pandoc(doc)
       local anchor = "endnote-" .. i
       local backlink = "ref-" .. i
       
-      -- Prepend the number and a backlink to the note content
       -- We want: 1. Note content...
-      
-      -- Create a list item for the note
-      -- We can use an OrderedList for automatic numbering
-      -- But to handle the content blocks correctly, let's just use blocks.
-      
-      -- Actually, putting them in an OrderedList is the cleanest way to format them.
-      -- But we need to ensure the blocks inside are handled.
       -- note_content is a list of blocks.
-      
-      -- Let's try to make it a simple OrderedList
       note_list:insert(note_content)
     end
     
